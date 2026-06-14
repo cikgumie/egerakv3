0706c59b4dff100f9d470a78cba654f42277bc7c
helmi <cikguhelmi83@gmai.com>
2026-06-14 23:03:52 +0800

    feat: convert site to PWA for mobile installation

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'smarttracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './favicon.png',
+  './icon-192.png',
+  './icon-512.png',
+  './manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        // Fetch fresh in background
+        fetch(e.request).then((networkResponse) => {
+          if (networkResponse.status === 200) {
+            caches.open(CACHE_NAME).then((cache) => cache.put(e.request, networkResponse));
+          }
+        }).catch(() => {/* Ignore network errors when offline */});
+        return cachedResponse;
+      }
+      return fetch(e.request);
+    })
+  );
+});
